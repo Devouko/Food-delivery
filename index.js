ac7bf22764d546713c670292bd645b86cbfd2967
Devouko <130544445+Devouko@users.noreply.github.com>
2023-08-14 11:11:30 +0300

    intial commit
    a food delivery web app

---
+++ index.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var ejs = require("ejs");
+var bodyParser = require("body-parser");
+var mysql = require("mysql");
+
+mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "node_project"
+})
+
+
+
+var app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.listen(8080);
+app.get('/', function (req, res) {
+
+
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "node_project"
+
+    })
+    con.query("SELECT * FROM products", (err, result) => {
+
+        res.render("pages/index", { result: result });
+    })
+
+});
